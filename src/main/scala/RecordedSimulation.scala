36e5483be705d0ac4d4cb7cfbf5d253e9762b5ac
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2020-02-05 00:01:37 +0100

    Changes to add a first version of a Gatling Test (It doesn't work yet).

---
+++ src/main/scala/RecordedSimulation.scala
@@ -0,0 +1,40 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class RecordedSimulation extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://www.menasoft.es")
+		.inferHtmlResources()
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3")
+		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0")
+
+	val headers_0 = Map("Upgrade-Insecure-Requests" -> "1")
+
+	val headers_2 = Map("Accept" -> "text/css,*/*;q=0.1")
+
+
+
+	val scn = scenario("RecordedSimulation")
+		.exec(http("request_0")
+			.get("/")
+			.headers(headers_0))
+		.pause(2)
+		.exec(http("request_1")
+			.get("/2019/09/15/primera-entrada/")
+			.headers(headers_0)
+			.resources(http("request_2")
+			.get("/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/styles/shCore.css?ver=3.0.9b")
+			.headers(headers_2),
+            http("request_3")
+			.get("/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/styles/shThemeDefault.css?ver=3.0.9b")
+			.headers(headers_2)))
+
+	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+}
